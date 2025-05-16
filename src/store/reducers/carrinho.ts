bcb1bbc993471e613d3ad6fb2352dbe693a7b8d9
Phillip <contato.phillip.menezes@gmail.com>
2025-05-16 19:35:28 -0300

    #0001 - Redux added

---
+++ src/store/reducers/carrinho.ts
@@ -0,0 +1,25 @@
+import { createSlice, PayloadAction } from '@reduxjs/toolkit'
+import { Produto } from '../../App'
+
+type CarrinhoState = {
+  itens: Produto[]
+}
+const initialState: CarrinhoState = {
+  itens: []
+}
+const carrinhoSlice = createSlice({
+  name: 'carrinho',
+  initialState,
+  reducers: {
+    add: (state, action: PayloadAction<Produto>) => {
+      const produto = action.payload
+      if (state.itens.find((p) => p.id === produto.id)) {
+        alert('Item já adicionado')
+      } else {
+        state.itens.push(produto)
+      }
+    }
+  }
+})
+export const { add } = carrinhoSlice.actions
+export default carrinhoSlice.reducer
